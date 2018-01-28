0a54814eaf3740926e6c904bcd0aa24573e98189
Sangeeta Jadoonanan <sjbitcode@gmail.com>
2018-01-28 17:21:16 -0500

    Use apiview

--- crypto/prices/views.py
+++ crypto/prices/views.py
@@ -1,20 +1,19 @@
-import json
-
 from django.http import HttpResponse
-from django.utils.decorators import method_decorator
-from django.views.decorators.csrf import csrf_exempt
-from django.views.generic import View
+
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from cryptocompare.main import get_prices
 
 
 def index(request):
-	return HttpResponse('Hello, world. You are at the prices app index.')
+    return HttpResponse('Hello, world. You are at the prices app index.')
 
 
-@method_decorator(csrf_exempt, name='dispatch')
-class CryptoView(View):
+class CryptoView(APIView):
+    def post(self, request):
+        base = request.data.get('base', '')
+        quote = request.data.get('quote', '')
 
-	def post(self, request):
-		content = get_prices('ETH', 'BTC')
-		return HttpResponse(json.dumps(content), content_type='application/json')
+        content = get_prices(base, quote)
+        return Response(content)
